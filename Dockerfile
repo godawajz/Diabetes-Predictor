edcfc4c55881122eaf5a55c8731189009db647b4
godawajz <godawajz@g.cofc.edu>
2025-10-29 01:35:38 -0400

    Added additional comments

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,15 @@ WORKDIR /app
 RUN apt-get update && apt-get install -y \
     && rm -rf /var/lib/apt/lists/*
 
+# Install project requirements
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Obtain a copy of the python code and the diabetes model.
 COPY app/ ./app/
 COPY models/ ./models/
 
+# Container expected to listen on port 8000.
+# Verify successful connection through https://localhost:8000/
 EXPOSE 8000
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- train_model.py
+++ train_model.py
@@ -5,25 +5,31 @@ from sklearn.ensemble import RandomForestRegressor
 import pickle
 import os
 
+DEBUG:bool = True # Toggles extra print messages used during development.
 
+# ============================ Load dataset ============================
 diabetes = load_diabetes()
 X, y = diabetes.data, diabetes.target
 
-print("Dataset loaded.")
-print(f"Dataset shape: {X.shape}")
-print(f"Features: {diabetes.feature_names}")
-print(f"Target range: {y.min():.1f} to {y.max():.1f}")
+if DEBUG:
+    print("Dataset loaded.")
+    print(f"Dataset shape: {X.shape}")
+    print(f"Features: {diabetes.feature_names}")
+    print(f"Target range: {y.min():.1f} to {y.max():.1f}")
 
-
-print("Sample preparation.")
+# ============================ Prep dataset ============================
+if DEBUG:
+    print("Sample preparation.")
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+if DEBUG:
+    print(f"Training samples: {X_train.shape[0]}")
+    print(f"Test samples: {X_test.shape[0]}")
 
-print(f"Training samples: {X_train.shape[0]}")
-print(f"Test samples: {X_test.shape[0]}")
-
-print("Training model.")
+# ============================= Train Model =============================
+if DEBUG:
+    print("Training model.")
 model = RandomForestRegressor(
     n_estimators=100,
     random_state=42,
@@ -31,15 +37,16 @@ model = RandomForestRegressor(
 )
 model.fit(X_train, y_train)
 
-
+# ============================= Eval Model =============================
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-print("Model evaluations.")
-print(f"Mean Squared Error: {mse:.2f}")
-print(f"R^2 Score: {r2:.3f}")
+if DEBUG:
+    print("Model evaluations.")
+    print(f"Mean Squared Error: {mse:.2f}")
+    print(f"R^2 Score: {r2:.3f}")
 
 os.makedirs('models', exist_ok=True)
 
